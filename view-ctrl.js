3f2889baa710d05f4e00006b66cb33d3856d609f
runhem <alexandrarunhem@n167-p120.eduroam.kth.se>
2017-10-16 18:02:42 +0200

    Trying out timeout with different views. Merging Arvids work

--- view-ctrl.js
+++ view-ctrl.js
@@ -1,14 +1,60 @@
-var time = 7000; //amount of time the containter will be visable
+var time = 1000; //amount of time the containter will be visable
+
+var count = 1;
+
 
 function listToQuiz() {
 	$('.all-lists').hide();
 	$('.quizView').show();
 }
 
-setTimeout(listToQuiz, time); //hides container after 7000 ms
 
+$('#quizForm').submit(function(){
+	$('.quizView').hide();
+})
 
-//ADD: Submit on Quiz-view - Show List, hide quiz
+function nextQuiz(){
 
+	console.log(count);
+	
+
+	if(count == 1){
+		$('.shopping-list-1').show();
+		$('middleView').hide();
+		setTimeout(listToQuiz,time)//hides container after 7000 ms
+	}
+	else if(count == 2){
+		$('.shopping-list-2').show();
+		$('middleView').hide();
+		setTimeout(listToQuiz,time)
+	}
+	else if(count == 3){
+		$('.shopping-list-3').show();
+		$('middleView').hide();
+		setTimeout(listToQuiz,time)
+	}
+	else if(count == 4){
+		$('.shopping-list-4').show();
+		$('middleView').hide();
+		setTimeout(listToQuiz,time)
+	}
+	else{
+		//show end-screen
+	}
+	count = count+1;
+}
+
+function showHoldUp(){
+	$('middleView').show();
+
+}
+
+$("button").click(function(event){
+    event.preventDefault();
+    var chosenItems = $("#quizForm input:checkbox:checked").map(function(){
+      return $(this).val();
+    }).get();
+    console.log(chosenItems); // chosenItems är en array som har alla valda items
+});
 
 
