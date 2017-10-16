18970deb43904cbad13b743be42ea17a73c236e6
arvidlarzzon <arvidlarzzon@hotmail.com>
2017-10-16 14:06:51 +0200

    more images and items done in model

--- model.js
+++ model.js
@@ -43,10 +43,86 @@ function saveResult(subjectId, colorType, chosenObjects, listItems){
 
 
 var items = [
-	egg = {
+	{
 		id: 1,
 		name: 'Eggs',
 		image: 'images/agg.jpg'
+	},
+	{
+		id: 2,
+		name: 'Apple',
+		image: 'images/apple.jpg'
+	},
+	{
+		id: 3,
+		name: 'Cucumber',
+		image: 'images/cucumber.jpg'
+	},
+	{
+		id: 4,
+		name: 'Juice',
+		image: 'images/juice.jpg'
+	},
+	{
+		id: 5,
+		name: 'Meat',
+		image: 'images/meat.jpg'
+	},
+	{
+		id: 6,
+		name: 'Milk',
+		image: 'images/milk.jpg'
+	},
+	{
+		id: 7,
+		name: 'Pear',
+		image: 'images/pear.jpg'
+	},
+	{
+		id: 8,
+		name: 'Sundried'
+		image: 'images/sundried.jpg'
+	},
+	{
+		id: 9,
+		name: 'Banana',
+		image: 'images/banana.jpg'
+	},
+	{
+		id: 10,
+		name: 'Butter',
+		image: 'images/butter.jpg'
+	},
+	{
+		id: 11,
+		name: 'Cheese',
+		image: 'images/cheese.jpg'
+	},
+	{
+		id: 12,
+		name: 'Tomato',
+		image: 'images/tomato.jpg'
+	},
+	flour: {
+		id: 13,
+		name: 'Flour',
+		image: 'images/flour.jpg'
+	},
+	{
+		id: 14,
+		name: 'Sugar',
+		image: 'images/sugar.jpg'
+	},
+	{
+		id: 15,
+		name: 'Tooth Paste',
+		image: 'images/toothpaste.jpg'
+	},
+	{
+		id: 16,
+		name: 'Fish',
+		image: 'images/fish.jpg'
 	}
 ]
+
 console.log(items[0])
